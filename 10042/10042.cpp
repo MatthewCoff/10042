f2a394f97ed3f79ef2d28469975782cc3efe3e21
MatthewCoff <miramavs@gmail.com>
2023-04-10 20:19:00 +0300

    Добавьте файлы проекта.

---
+++ 10042/10042.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <Windows.h>
+using namespace std;
+class Vse {
+
+
+class FileWork
+{
+public:
+	bool readFile(string file, string& infor)
+	{
+		cout << "read file" << endl;
+
+	}
+
+	bool writeFile(string file, const string& infor)
+	{
+		cout << "запись файла" << endl;
+
+	}
+
+	bool deleteFile(string file)
+	{
+		cout << "видалення файла" << endl;
+
+	}
+};
+
+
+class DataProcessing
+{
+public:
+	bool RealStructur(const string& infor, vector<string>& data)
+	{
+		cout << "реализация структурирования информации из строки  " << endl;
+
+	}
+
+	bool RealObrab(vector<string>& data)
+	{
+		cout << "реализация обработки " << endl;
+	}
+};
+
+
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	Vse vs;
+	
+
+}
